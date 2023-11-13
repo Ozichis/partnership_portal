08884a57847b37136ffa30c5b961037a920b311a
Ozichis <73189626+Ozichis@users.noreply.github.com>
2023-11-13 17:52:02 +0100

    Delete main directory

--- main/helpers.py
+++
@@ -1,5 +0,0 @@
-from models import User
-from django.conf import settings
-
-users = User.objects.filter(user_type=1)
-print(len(users))
--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-app_name = 'main'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.acc_login, name='login'),
-    path('logout/', views.acc_logout, name='logout'),
-]
